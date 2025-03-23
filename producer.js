5ffb566a03533ea0259ed6252cf3ccfe97f907bf
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2025-03-23 11:51:55 +0530

    study : added consumer and was able to add 2 partitions and was able to do rebalancing

--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const {kafka} = require("./client")
+const readline = require("readline")
+
+const rl  = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+})
 
 async function init(){
     const producer = kafka.producer();
@@ -6,18 +12,27 @@ async function init(){
     await producer.connect();
     console.log("Producer Connected Successfully..")
 
-    await producer.send({
-        topic : "rider-updates",
-        messages : [
-            {
-                partition : 0,
-                key : "location-update" , value : JSON.stringify({name : "Tony Stark", loc : "SOUTH"}),
-            },
-
-        ]
-    });
-
-    await producer.disconnect()
+    rl.setPrompt("> ")
+    rl.prompt();
+    rl.on("line", async function(line) {
+        const [riderName, location] = line.split(" ")
+        // console.log(location === "north")
+        await producer.send({
+            topic : "rider-updates",
+            messages : [
+                {
+                    partition : location.toLowerCase() === "north" ? 0 : 1,
+                    key : "location-update" , value : JSON.stringify({name : riderName, loc : location}),
+                },
+    
+            ]
+        });
+    
+    }).on("close", async ()=>{
+        await producer.disconnect()
+    })
+  
+    
 }
 
 init();
